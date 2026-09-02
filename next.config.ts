e07a97ad0bc90ed4288b6999e5b62354ac3f59dd
LucAlphonze <lucasalfonzo19@gmail.com>
2026-09-01 21:06:24 -0300

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+allowedDevOrigins:['192.168.100.9'],
+  images:{
+    remotePatterns:[
+      {
+        hostname:'images.unsplash.com',
+        protocol:'https',
+        port:'',
+      }
+    ]
+   }
+  
+}
 
 export default nextConfig;
