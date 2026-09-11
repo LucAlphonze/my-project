d5b0fe5160f835e60e254dc542e6235fab489000
LucAlphonze <lucasalfonzo19@gmail.com>
2026-09-10 21:03:55 -0300

    added-video

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+'use client'
+
 import Features from "@/components/features-3";
-import HeroSection from "@/components/hero-section-1";
+import HeroSection from "@/components/hero-section-5";
+import StatsSection from "@/components/stats-1";
+import StatsSection2 from "@/components/stats2";
+import { useRef } from "react";
 
 export default function Home() {
+  const targetRef = useRef<HTMLElement>(null);
   return <div>
-    <HeroSection/>  
-    <Features/> 
+    <HeroSection onClick={() => targetRef.current?.scrollIntoView({ behavior: "smooth" })}/>  
+    <Features ref={targetRef}/> 
+    <StatsSection/>
+    <StatsSection2/>
+    <StatsSection/>
+    <StatsSection2/>
   </div>
 }
